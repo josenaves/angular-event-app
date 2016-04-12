2a3343463894c3d7cb554a719a614abb249735ed
José Naves <josenaves@gmail.com>
2016-04-12 15:41:45 -0300

    Using  instead

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,14 +4,13 @@ eventsApp.controller('EventController',
 	// Injeting services $scope and eventData into controller; $log too
 	function EventController($scope, eventData, $log) {
 
+		//$scope.query = "";
 		$scope.sortorder = 'name';
-		$scope.query = "";
 		
 		eventData.getEvent()
-			.success(function(event) { $scope.event = event; })
-			.error(function(data, status, headers, config) {
-				$log.warn(data, status, headers, config);
-			});
+			.$promise
+			.then(function(event) { $scope.event = event; console.log(event);})
+			.catch(function(response) { console.log(response); });
 
 		$scope.upVoteSession = function(session) {
 			session.upVoteCount++;
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,10 +1,10 @@
 'use strict';
-// We are injecting the $http built-in service into our service
-eventsApp.factory('eventData', function($http) {
+// We are injecting the $resource built-in service into our service
+eventsApp.factory('eventData', function($resource) {
 	return {
 		getEvent: function() {
 			// now we are returning a promisse
-			return $http({method: 'GET', url: '/data/event/1'});
+			return $resource('/data/event/:id', {id: '@id'}).get({id:1});
 		}
 	};
 });
