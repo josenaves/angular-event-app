d131f37e1788948e4a895516910ddc5325167a4d
José Naves <josenaves@gmail.com>
2016-04-12 17:11:39 -0300

    Add  service sample

---
+++ app/js/controllers/LocaleSampleController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+eventsApp.controller('LocaleSampleController',
+	// We injected $locale service 
+	function LocaleSampleController($scope, $locale) {
+
+		console.log($locale);
+		$scope.myDate = Date.now();
+		$scope.myShortFormat = $locale.DATETIME_FORMATS.shortDate;
+		$scope.myFullFormat = $locale.DATETIME_FORMATS.longDate;
+	}
+); 
