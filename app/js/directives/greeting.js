f52f7217ac0c4b24c4ead9dd3024878bbe0d42a0
José Naves <josenaves@gmail.com>
2016-04-17 20:13:57 -0300

    Add controller sharing between directives

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -5,14 +5,34 @@ eventsApp.directive('greeting', function() {
 		restrict: 'E',
 		replace: true,
 		template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
-		controller: 'GreetingController'
+		controller: function($scope) {
+			var greetings = ['hello'];
+			$scope.sayHello = function() {
+				alert(greetings.join());
+			}
+			this.addGreeting = function(greeting) {
+				greetings.push(greeting);
+			}
+		}
 	};
-});
 
-eventsApp.controller('GreetingController', 
-	function GreetingController($scope) {
-		$scope.sayHello = function() {
-			alert('Hello');
+})
+.directive('finnish', function() {
+	return {
+		restrict: 'A',
+		require: 'greeting',
+		link: function($scope, element, attrs, controller) {
+			controller.addGreeting('hei');
 		}
 	}
-);
+})
+.directive('hindi', function() {
+	return {
+		restrict: 'A',
+		require: 'greeting',
+		link: function($scope, element, attrs, controller) {
+			controller.addGreeting('namaste');
+		}
+	}
+});
+
